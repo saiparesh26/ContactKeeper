1af0f730fb0e03cc7ddac22592965b1f52ac2bc9
Sai Paresh <sai.paresh26@gmail.com>
2020-04-17 12:05:04 +0530

    Routing complete in backend

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
+
+
+// Connect Database
+connectDB();
+
+//init Middleware 
+app.use(express.json({ extended: false}));
 
 app.get('/' , (req, res) => {
     res.send('Hello world');
